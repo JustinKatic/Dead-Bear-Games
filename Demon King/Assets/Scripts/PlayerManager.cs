3a66ca542009586305083a390715ef9ea5216fd8
Justin <justinkatic4@gmail.com>
2021-07-03 19:07:15 +1000

    removed demo projects added player instantiate

---
+++ Demon King/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class PlayerManager : MonoBehaviour
+{
+    PhotonView PV;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    private void Start()
+    {
+        if (PV.IsMine)
+            Createontroller();
+    }
+
+    void Createontroller()
+    {
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), Vector3.zero, Quaternion.identity);
+    }
+
+}
